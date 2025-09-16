081cb82623670512d379c602353bf417c7b5f7c9
mr_sarathi <mrblacksarathi@gmail.com>
2025-09-16 12:28:55 +0530

    Time: 1 ms (84.25%), Space: 41.2 MB (20.98%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,19 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int) Math.sqrt(x);
+        if(x<2)return x;
+        int strt=1;
+        int end=x/2;
+        int ans=0;
+        while(strt<=end){
+            int mid=strt+(end-strt)/2;
+            if((long)mid*mid<=x){
+                ans=mid;
+                strt=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return ans;
     }
 }
