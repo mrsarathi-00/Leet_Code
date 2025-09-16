a1052454eb8bd0fd4288098ad93bfd6c745a15ff
mr_sarathi <mrblacksarathi@gmail.com>
2025-09-16 10:33:00 +0530

    Time: 0 ms (100%), Space: 40.4 MB (85.46%) - LeetHub

--- 1674-minimum-operations-to-make-array-equal/1674-minimum-operations-to-make-array-equal.java
+++ 1674-minimum-operations-to-make-array-equal/1674-minimum-operations-to-make-array-equal.java
@@ -1,14 +1,10 @@
 class Solution {
     public int minOperations(int n) {
-        int arr[]=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=(2*i)+1;
+        if(n%2==0){
+            return (n*n)/4;
         }
-        int target=n;
-        int cnt=0;
-        for(int i=0;i<n/2;i++){
-           cnt+=target-arr[i];
+        else{
+            return (n*n-1)/4;
         }
-        return cnt;
     }
 }
